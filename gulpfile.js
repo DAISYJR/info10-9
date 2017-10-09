eb94e3e247cccecade04565d61185a637177931f
DAISYJR <daisyzhuan1125@163.com>
2017-10-09 09:50:15 +0800

    again login

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,18 @@ var concat = require('gulp-concat');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
-gulp.task('default',['cssFile','jsFile']);
-gulp.task('cssFile',function(){
-    gulp.src('./css/*')
-        .pipe(concat('./css/*'))
-        .pipe(rename('./all.css'))
-        .pipe(minifyCss('./.min.css'))
+gulp.task('default', ['cssFile', 'jsFile']);
+gulp.task('cssFile', function () {
+    gulp.src('./css/*.css')
+        .pipe(concat('./all.css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifyCss())
         .pipe(gulp.dest('./css'))
 });
-gulp.task('jsFile',function(){
-    gulp.src('./js/*')
-        .pipe(rename('./all.js'))
-        .pipe(uglify('./.min.js'))
+gulp.task('jsFile', function () {
+    gulp.src('./js/*.js')
+        .pipe(concat('./all.js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
         .pipe(gulp.dest('./js'))
 });
